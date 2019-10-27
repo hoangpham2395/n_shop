92841620139bc4fe16de6c5ec2c26e9b67a941f6
HoangPH <hoangpham2395@gmail.com>
2019-10-27 22:14:56 +0700

    [Frontend] Upddate seach in header

--- resources/views/layouts/frontend/layouts/header.blade.php
+++ resources/views/layouts/frontend/layouts/header.blade.php
@@ -25,15 +25,14 @@
                 <nav class="menu">
                     {{-- Search --}}
                     <ul class="main_menu text-center">
-                            <div class="search-product pos-relative bo4 of-hidden m-t-22">
-                                <Form>
+                        <div class="search-product pos-relative bo4 of-hidden m-t-22">
+                            {!! Form::open(['route' => 'frontend.products.index', 'method' => 'GET', 'id' => 'form_header_search']) !!}
                                 {!! Form::text('product_name', Request::get('product_name'), ['class' => 's-text7 size6 p-l-23 p-r-50', 'placeholder' => 'Bạn cần tìm gì hôm nay?']) !!}
-                                <button type="button" class="flex-c-m size5 ab-r-m color2 color0-hov trans-0-4">
+                                <button type="button" class="flex-c-m size5 ab-r-m color2 color0-hov trans-0-4" onclick="$('form#form_header_search').submit();">
                                     <i class="fs-12 fa fa-search" aria-hidden="true"></i>
                                 </button>
-                                </Form>
-                            </div>
-
+                            {!! Form::close() !!}
+                        </div>
                     </ul>
 
                     {{-- Menu --}}
