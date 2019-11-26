ab0cdc8f87ad3560268988811b7b3d2be9ff9890
HoangPH <hoangpham2395@gmail.com>
2019-11-26 19:35:05 +0700

    [Backend] Update password

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -3,26 +3,38 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Base\BaseController;
 use App\Repositories\AdminRepository;
-use Illuminate\Support\Facades\Input;
 use App\Http\Requests\Backend\AdminRequest;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\MessageBag;
-use Illuminate\Http\Request;
 
-class AdminController extends BaseController 
+/**
+ * Class AdminController
+ * @package App\Http\Controllers\Backend
+ */
+class AdminController extends BaseController
 {
-	public function __construct(AdminRepository $adminRepository) 
+    /**
+     * AdminController constructor.
+     * @param AdminRepository $adminRepository
+     */
+    public function __construct(AdminRepository $adminRepository)
 	{
 		$this->setRepository($adminRepository);
 		parent::__construct();
 	}
 
-	public function request() 
+    /**
+     * @return string
+     */
+    public function request()
 	{
 		return AdminRequest::class;
 	}
 
-	public function index() 
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View|void
+     */
+    public function index()
 	{
 		if (!$this->_checkPermission()) {
 			return abort('404');
@@ -31,7 +43,10 @@ class AdminController extends BaseController
 		return parent::index();
 	}
 
-	public function create() 
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View|void
+     */
+    public function create()
 	{
 		if (!$this->_checkPermission()) {
 			return abort('404');
@@ -40,7 +55,11 @@ class AdminController extends BaseController
 		return parent::create();
 	}
 
-	public function store(AdminRequest $request) 
+    /**
+     * @param AdminRequest $request
+     * @return \Illuminate\Http\RedirectResponse|void
+     */
+    public function store(AdminRequest $request)
 	{
 		if (!$this->_checkPermission()) {
 			return abort('404');
@@ -49,7 +68,11 @@ class AdminController extends BaseController
 		return $this->storeBase();
 	}
 
-	public function edit($id) 
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View|void
+     */
+    public function edit($id)
 	{
 		$entity = $this->getRepository()->findById($id);
 
@@ -60,7 +83,12 @@ class AdminController extends BaseController
 		return view('backend.admin.edit', compact('entity'));
 	}
 
-	public function update(AdminRequest $request, $id) 
+    /**
+     * @param AdminRequest $request
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse|void
+     */
+    public function update(AdminRequest $request, $id)
 	{
 		$entity = $this->getRepository()->findById($id);
 
@@ -77,7 +105,9 @@ class AdminController extends BaseController
 
 		if (empty($data['password'])) {
 			unset($data['password']);
-		}
+		} else {
+		    $data['password'] = bcrypt($data['password']);
+        }
 
 		unset($data['confirm_password']);
 
@@ -92,11 +122,15 @@ class AdminController extends BaseController
 			logError($e);
 			DB::rollBack();
 		}
-		
+
 		return redirect()->route('backend.admin.index')->withErrors(new MessageBag(['update_failed' => getMessage('update_failed')]));
 	}
 
-	public function destroy($id) 
+    /**
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse|void
+     */
+    public function destroy($id)
 	{
 		$entity = $this->getRepository()->findById($id);
 
@@ -113,7 +147,10 @@ class AdminController extends BaseController
 		return redirect()->route('backend.admin.index')->withErrors(new MessageBag(['delete_failed' => getMessage('delete_failed')]));
 	}
 
-	protected function _checkPermission() 
+    /**
+     * @return mixed
+     */
+    protected function _checkPermission()
 	{
 		return backendGuard()->user()->isSuperAdmin();
 	}
